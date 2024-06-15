b65cc853c0adbbdfa5a8b5152d669f1929f7bea3
Faizal <74812515+ioresolution@users.noreply.github.com>
2024-06-15 11:24:25 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dotfiles
+### Note: DO NOT USE
+Take my heed. Or expect things or two to break(?) 
+## whatis this
+Collection of my .config directory and such. Will include but not limited to:
+- xfce4 and goodies
+- fish
+- neofetch
+- conky
+- xfce4-term
+ 
+![](https://github.com/ioresolution/dotfiles/blob/main/preview.png)
---
+++ dotfiles.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "NEEDed packages: "
+echo -e "'fish' 'conky' 'xfce4' "
+while true; do
+    read -p "Install? y/n " yn
+    case $yn in
+        [Yy]* ) sudo apt install fish conky xfce4 xfce4-goodies;  break;;
+        [Nn]* ) echo "proceeding"; sleep 2s; break;;
+   esac
+done
+echo 'copying directories' 
+cp -r .config $HOME
+cp walp.png $HOME
+sudo  mkdir /etc/conky && sudo cp conky.conf /etc/conky/ 
+echo -e "done.\n\n\n[signing out.]\n\n\nexit using CTRL + C\n\n"
+xfce4-panel -r; xfwm4 --replace 
+
