c56f9db43dcff4cd3a640ea117bf33004bb80792
Faizal <74812515+ioresolution@users.noreply.github.com>
2024-06-17 10:00:26 +0000

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 echo "NEEDed packages: "
-echo -e "'fish' 'conky' 'xfce4' 'wal'"
+echo -e "'fish' 'conky' 'xfce4'"
 while true; do
     read -p "Install? y/n " yn
     case $yn in
-        [Yy]* ) sudo apt install fish conky xfce4 xfce4-goodies python3 python3-pip;  pip install pywal --break-system-packages; break;;
-        [Nn]* ) echo "proceeding"; sleep 2s; break;;
+        [Yy]* ) sudo apt install fish conky xfce4 xfce4-goodies; echo "proceeding"; break;;
+        [Nn]* ) echo "proceeding"; break;;
    esac
 done
+sleep 2s
 echo 'copying directories' 
 cp -r .config $HOME
-cp walp.png $HOME
+cp walp.png $HOME/Downloads
 sudo  mkdir /etc/conky && sudo cp conky.conf /etc/conky/ 
-echo -e "done.\n\n\n[signing out.]\n\n\nexit using CTRL + C\n\n"
-xfce4-panel -r; xfwm4 --replace 
+echo -e "done.\n[signing out.]\n\n\nexit using CTRL + C\n"
+xfce4-panel -r && xfwm4 --replace 
