403aa695285bd506eb0b2c1ad378aacb51740e13
Faizal <74812515+ioresolution@users.noreply.github.com>
2024-06-25 11:50:37 +0600

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,12 +4,10 @@ echo -e "'neofetch' 'fish' 'conky'"
 while true; do
     read -p "Install? y/n " yn
     case $yn in
-        [Yy]* ) sudo apt install neofetech fish conky; echo "proceeding"; break;;
+        [Yy]* ) sudo apt install neofetch fish conky; echo "proceeding"; break;;
         [Nn]* ) echo "proceeding"; break;;
    esac
 done
-sleep 2s
 echo 'copying directories' 
 cp -r .config $HOME
-cp walp.png $HOME/Downloads
 sudo  mkdir /etc/conky && sudo cp conky.conf /etc/conky/ 
