c605be38a9a9e0bce835486bb89b7f92ed7a2fcf
Faizal <74812515+ioresolution@users.noreply.github.com>
2024-06-25 11:45:51 +0600

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "NEEDed packages: "
-echo -e "'fish' 'conky' 'xfce4'"
+echo -e "'neofetch' 'fish' 'conky'"
 while true; do
     read -p "Install? y/n " yn
     case $yn in
-        [Yy]* ) sudo apt install fish conky xfce4 xfce4-goodies; echo "proceeding"; break;;
+        [Yy]* ) sudo apt install neofetech fish conky; echo "proceeding"; break;;
         [Nn]* ) echo "proceeding"; break;;
    esac
 done
@@ -13,5 +13,3 @@ echo 'copying directories'
 cp -r .config $HOME
 cp walp.png $HOME/Downloads
 sudo  mkdir /etc/conky && sudo cp conky.conf /etc/conky/ 
-echo -e "done.\n[signing out.]\n\n\nexit using CTRL + C\n"
-xfce4-panel -r && xfwm4 --replace 
